e7fa71b1079984ddef42e641489fee7c7f9f8b99
Murilo Zanini <carvalho.zanini@gmail.com>
2019-03-11 12:50:12 -0300

    Projeto criado com o IntelliJ o melhor do mundo

---
+++ pratica01/src/com/company/MeuCadastro.java
@@ -0,0 +1,39 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class MeuCadastro {
+    public static void main(String[] args) {
+        String nome, funcao;
+        int idade;
+        double salario;
+        //Pega os dados do teclado
+        Scanner scanner = new Scanner(System.in);
+        boolean continuar = true;
+        while(continuar) {
+            System.out.println("Informe o nome do usuário:");
+            nome = scanner.nextLine();
+            System.out.println("Informe sua função:");
+            funcao = scanner.nextLine();
+            System.out.println("Informe sua idade:");
+            idade = scanner.nextInt();
+            System.out.println("Informe seu salário:");
+            salario = scanner.nextDouble();
+            System.out.printf(
+                    "Nome: %s\nFunção:%s\nIdade:%d\nSalário: R$%.2f",
+                    nome,   //Nome do usuário
+                    funcao, //Cargo do usuário
+                    idade,  //Idade do usuario
+                    salario //Salário do usuário
+            );
+            String resposta;
+            System.out.println("Deseja continuar?");
+            resposta = scanner.next();
+            if(resposta.equalsIgnoreCase("SIM"))
+                continuar = true;
+            else
+                continuar = false;
+        }
+
+    }
+}
