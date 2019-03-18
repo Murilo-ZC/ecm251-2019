152d05450c5cbe26f13ac63e1a86f12a70be3869
Murilo Zanini <carvalho.zanini@gmail.com>
2019-03-18 11:43:29 -0300

    Projeto criado com Eclipse

---
+++ pratica02/eclpseAqui/olaMundoEclipse/src/olaMundoEclipse/MinhaClasseJava.java
@@ -0,0 +1,14 @@
+package olaMundoEclipse;
+
+public class MinhaClasseJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int idade = 24;
+		String nome = "Emerson";
+		System.out.println("Nome: " + nome);
+		System.out.println("Idade: " + idade);
+
+	}
+
+}
