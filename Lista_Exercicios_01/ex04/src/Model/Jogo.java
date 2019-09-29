584af2847d62ec6639a9e6a7e0b015b436cfa120
Murilo Zanini de Carvalho <murilo@MacBook-Pro-de-Murilo.local>
2019-09-29 18:28:52 -0300

    Solução 3

---
+++ Lista_Exercicios_01/ex04/src/Model/Jogo.java
@@ -0,0 +1,25 @@
+package Model;
+
+public class Jogo {
+    public String nome, lancamento, genero, plataforma;
+    public int id;
+
+    public Jogo(String nome, String lancamento, String genero, String plataforma, int id) {
+        this.nome = nome;
+        this.lancamento = lancamento;
+        this.genero = genero;
+        this.plataforma = plataforma;
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Jogo{" +
+                "nome='" + nome + '\'' +
+                ", lancamento='" + lancamento + '\'' +
+                ", genero='" + genero + '\'' +
+                ", plataforma='" + plataforma + '\'' +
+                ", id=" + id +
+                '}';
+    }
+}
